5608925e0fd0d408ebc3a55b835cc193a7dd93e6
Victor Zhang <victorzhang@pc-212-78.customer.sfields.nido.lan>
2018-10-16 16:03:57 +0100

    update the answer for q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -4,7 +4,72 @@ public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		// TODO Auto-generated method stub
+		try{
+			int n=portfolios.length;
+			int r=2;
+			int totalWays=factorial(n)/(factorial(r)*factorial(n-r));
+			int[] a =new int[totalWays];
+			int index=0;
+			for(int i=0;i<portfolios.length-i;i++){
+				for(int j=i+1;j<portfolios.length;i++){
+					a[index++]=convertToDecimal(convertToBinary(portfolios[i]),convertToBinary(portfolios[j]));
+				}
+			}
+			return findMax(a);
+		}catch(Exception e){
+		}
+		
 		return -1;
 	}
-
+	//converting each decimal number to 16-bit binary and return back the binary in form of an array
+	public static int[] convertToBinary(int n){
+		int[] b = new int[16];
+		int i=0;
+		while(n > 0){
+			b[i++] = n%2;
+			n = n/2;
+		}
+		return b;
+	}
+	// Merging two binary arrays into one binary in 'String' type and finally convert the merged binary to decimal.
+	public static int convertToDecimal(int[] binarya, int[] binaryb){
+		String newBinary = "";
+		for(int j=0;j<16;j++){
+			if(mergingTo1(binarya[j],binaryb[j])){
+				newBinary+="1";
+			}else{
+				newBinary+="0";
+			}
+		}
+		return Integer.parseInt(newBinary,2);
+	}
+	//merging by using the given rules
+	public static boolean mergingTo1(int a, int b){
+		if(a==b){
+			return false;
+		}
+		return true;
+	}
+	public static int findMax(int[] a){
+		if(a.length<=0){
+			return -1;
+		}
+		int max=a[0];
+		int index=1;
+		while(index<a.length){
+			if(max<a[index]){
+				max=a[index];
+			}
+			index++;
+		}
+		return max;
+	}
+	//simulate factorial calculation in Math
+	public static int factorial(int n){
+		int f=1;
+		for(int i=1;i<=n;i++){
+			f*=i;
+		}
+		return f;
+	}
 }
